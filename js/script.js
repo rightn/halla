0d2bd4fb1139e7c88983a0bfdcb01e50d015bece
rightn <ygo9458@gmail.com>
2022-05-31 11:03:12 +0900

    최종본

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
-
+    
+    let headerBottom = $('.header-bottom');
+    let gnbBottomLiA = $('.gnb-bottom>li');
+    $.each(gnbBottomLiA, function(index, item){
+        $(this).mouseenter(function(){
+            headerBottom.addClass('header-bottom-active');
+        });
+        $(this).mouseleave(function(){
+            headerBottom.removeClass('header-bottom-active');
+        });
+    })
 });
 window.onload = function () {
 
     // 비주얼슬라이드
     let sw_visual = new Swiper('.sw-visual', {
         loop: true,
+        speed: 1000,
         navigation: {
-            prevEl: 'sw-visual-bt-prev',
-            nextEl: 'sw-visual-bt-next'
+            prevEl: '.sw-visual-bt-prev',
+            nextEl: '.sw-visual-bt-next'
         },
         pagination: {
             el: ".swiper-pagination"
